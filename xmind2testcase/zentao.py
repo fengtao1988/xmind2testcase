00185756dabb0904865aa8f8ff56a52af70357e5
fengtao <fengtao@roobo.com>
2019-06-04 11:56:22 +0800

    1.修改测试用例csv title 2.适配metadata中testcase字典修改

--- xmind2testcase/zentao.py
+++ xmind2testcase/zentao.py
@@ -18,7 +18,7 @@ def xmind_to_zentao_csv_file(xmind_file):
     logging.info('Start converting XMind file(%s) to zentao file...', xmind_file)
     testcases = get_xmind_testcase_list(xmind_file)
 
-    fileheader = ["所属模块", "用例标题", "前置条件", "步骤", "预期", "关键词", "优先级", "用例类型", "适用阶段"]
+    fileheader = ["所属模块", "用例标题", "前置条件", "步骤", "预期", "关键词", "优先级", "用例类型","用例状态"]
     zentao_testcase_rows = [fileheader]
     for testcase in testcases:
         row = gen_a_testcase_row(testcase)
@@ -29,7 +29,7 @@ def xmind_to_zentao_csv_file(xmind_file):
         logging.info('The zentao csv file already exists, return it directly: %s', zentao_file)
         return zentao_file
 
-    with open(zentao_file, 'w', encoding='utf8') as f:
+    with open(zentao_file, 'w', encoding='gbk',newline='') as f: # fengtao 1,modify encoding to GBK 2,add paremater newline=''
         writer = csv.writer(f)
         writer.writerows(zentao_testcase_rows)
         logging.info('Convert XMind file(%s) to a zentao csv file(%s) successfully!', xmind_file, zentao_file)
@@ -40,13 +40,14 @@ def xmind_to_zentao_csv_file(xmind_file):
 def gen_a_testcase_row(testcase_dict):
     case_module = gen_case_module(testcase_dict['suite'])
     case_title = testcase_dict['name']
-    case_precontion = testcase_dict['preconditions']
-    case_step, case_expected_result = gen_case_step_and_expected_result(testcase_dict['steps'])
-    case_keyword = '功能测试'
-    case_priority = gen_case_priority(testcase_dict['importance'])
-    case_type = gen_case_type(testcase_dict['execution_type'])
-    case_apply_phase = '迭代测试'
-    row = [case_module, case_title, case_precontion, case_step, case_expected_result, case_keyword, case_priority, case_type, case_apply_phase]
+    case_precondition = testcase_dict['preconditions']['precondition']
+    case_step, case_expected_result = gen_case_step_and_expected_result(testcase_dict['steps'],testcase_dict['expects'])
+    case_keyword = testcase_dict['execution_type']  # fengtao：indicate case is manual or auto exec
+    #case_priority = gen_case_priority(testcase_dict['importance']) # fengtao：zentao version is low,use number as priority
+    case_priority = testcase_dict['importance']
+    case_type = ''   # fengtao：to do
+    case_status = '正常'
+    row = [case_module, case_title, case_precondition, case_step, case_expected_result, case_keyword, case_priority, case_type,case_status]
     return row
 
 
@@ -59,20 +60,27 @@ def gen_case_module(module_name):
     return module_name
 
 
-def gen_case_step_and_expected_result(steps):
+def gen_case_precondition(testcase_dict):
+    return testcase_dict['preconditions']['precondition']
+
+
+def gen_case_step_and_expected_result(steps,expects):
     case_step = ''
     case_expected_result = ''
 
     for step_dict in steps:
-        case_step += str(step_dict['step_number']) + '. ' + step_dict['actions'].replace('\n', '').strip() + '\n'
-        case_expected_result += str(step_dict['step_number']) + '. ' + \
-            step_dict['expectedresults'].replace('\n', '').strip() + '\n' \
-            if step_dict.get('expectedresults', '') else ''
+        case_step += str(step_dict['step_number']) + '. ' + step_dict['action'].replace('\n', '').strip() + '\n'
+
+    for expect_dict in expects:
+        case_expected_result += str(expect_dict['expect_number']) + '. ' + \
+        expect_dict['expect'].replace('\n', '').strip() + '\n' \
+        if expect_dict.get('expect', '') else ''
+
 
     return case_step, case_expected_result
 
 
-def gen_case_priority(priority):
+def gen_case_priority(priority):   #  禅道版本太低，使用1/2/3/4 作为优先级，此方法目前不使用
     mapping = {1: '高', 2: '中', 3: '低'}
     if priority in mapping.keys():
         return mapping[priority]
@@ -80,7 +88,7 @@ def gen_case_priority(priority):
         return '中'
 
 
-def gen_case_type(case_type):
+def gen_case_type(case_type):   # 从case_dict 中的execution_type中获取，此方法不使用
     mapping = {1: '手动', 2: '自动'}
     if case_type in mapping.keys():
         return mapping[case_type]
